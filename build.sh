559fc3cced2bb440507713b71491a1ec54793824
warseytourism <git@warsey-tourism.com>
2022-08-16 15:55:04 +0300

    Tmp fix

--- build.sh
+++ build.sh
@@ -3,6 +3,8 @@
 echo "Building Warsey's Vendor System."
 mkdir ~/warseyapps-files
 mkdir ~/warseyapps-files/warseyvendor
-cp -r src/appdata/* ~/warseyapps-files/warseyvendor
-mkdir ~/warseyapps-files/warseyvendor/invitecodes
+if [ ! -d "~/warseyapps-files/warseyvendor/invitecodes" ]; then
+    mv src/appdata/* ~/warseyapps-files/warseyvendor
+    mkdir ~/warseyapps-files/warseyvendor/invitecodes
+fi
 docker-compose up -d
