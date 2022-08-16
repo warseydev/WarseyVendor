0654f0aa026e8b43755e9bea38d31df71bce87f0
warseytourism <git@warsey-tourism.com>
2022-08-16 15:24:15 +0300

    More Fixes

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 echo "Building Warsey's Vendor System."
+mkdir ~/warseyapps-files
+mkdir ~/warseyapps-files/warseyvendor
 cp -r src/appdata/* ~/warseyapps-files/warseyvendor
+mkdir ~/warseyapps-files/warseyvendor/invitecodes
 docker-compose up -d
